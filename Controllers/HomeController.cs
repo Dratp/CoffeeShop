1fcdbc3b72694f2ae7d7b7e363f0d7bb362d6019
David Heizer <DavidHeizer@QuickenLoans.com>
2020-09-02 08:27:21 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult NewUser(string firstName, string lastName, string eMail, string phone, string pass1, string pass2)
+        {
+            bool match = false;
+            ViewBag.Match = match;
+            ViewBag.Name = $"{firstName} {lastName}";
+            ViewBag.eMail = eMail;
+            match = pass1.Equals(pass2);
+
+            if (!match)
+            {
+                return Content($"The passwords do not match");
+                
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult PassNoMatch()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
