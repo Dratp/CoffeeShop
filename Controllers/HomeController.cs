1875758d3ab8d9a23582b93a4d135585e61db5b0
David Heizer <DavidHeizer@QuickenLoans.com>
2020-09-04 13:13:21 -0400

    PreAssessment Lab

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,23 +18,45 @@ namespace CoffeeShop.Controllers
             return View();
         }
 
+        public static bool ValidatePassword(string pass)
+        {
+            foreach(char let in pass)
+            {
+                if(let == '1' || let == '2' || let == '3' || let == '4' || let == '5' || let == '6' || let == '7' || let == '8' || let == '9' || let == '0')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        [HttpPost]
         public ActionResult NewUser(string firstName, string lastName, string eMail, string phone, string pass1, string pass2)
         {
             bool match = false;
+            bool validPass = true;
             ViewBag.Match = match;
             ViewBag.Name = $"{firstName} {lastName}";
             ViewBag.eMail = eMail;
             match = pass1.Equals(pass2);
 
+            validPass = ValidatePassword(pass1);
+
+            if (!validPass)
+            {
+                ViewBag.passStatus = "The password did not contain a number";
+                return View("Register");
+            }
+
             if (!match)
             {
-                return Content($"The passwords do not match");
-                
+                ViewBag.matchStatus = "The Passwords did not match";
+                return View("Register");
+                //return RedirectToAction("Register", "Home");
+                //return Content($"The passwords do not match");
             }
-            else
-            {
-                return View();
-            }
+
+            return View();
         }
 
         public ActionResult PassNoMatch()
